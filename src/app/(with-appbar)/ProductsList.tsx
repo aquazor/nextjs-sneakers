b005896dc4d0e745624479b492072dcba4f208a3
aquazor <kornevskiy99@gmail.com>
2025-02-04 00:39:43 +0200

    feat: implement getting photos for sneakers

--- src/app/(with-appbar)/ProductsList.tsx
+++ src/app/(with-appbar)/ProductsList.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useProductsContext } from './productsContext';
-import Picture from '@/assets/photo-01.png';
+import { useProductsContext } from '../../context/productsContext';
 import LoadingButton from '@/components/LoadingButton';
 
 export default function ProductsList() {
@@ -10,18 +9,18 @@ export default function ProductsList() {
 
   return (
     <div className="w-full">
-      <ul className="flex flex-wrap">
+      <ul className="flex flex-wrap justify-center min-[550px]:justify-stretch">
         {products.length === 0 && isLoading
           ? [...Array(5)].map((_, index) => (
               <li
                 className="p-2 w-full min-[550px]:w-1/2 md:w-1/3 xl:w-1/4 2xl:w-1/5 grid"
                 key={index}
               >
-                <div className="shadow-lg border border-primary flex flex-col animate-pulse">
-                  <div className="h-60 border-b border-primary" />
-                  <div className="p-2 space-y-3">
-                    <div className="h-6 border border-primary" />
-                    <div className="h-4 border w-20 border-primary" />
+                <div className="shadow-lg border border-border flex flex-col animate-pulse">
+                  <div className="h-72 border-b border-border bg-primary/10" />
+                  <div className="px-2 py-4 space-y-3">
+                    <div className="h-6 border border-border" />
+                    <div className="h-4 border w-20 border-border" />
                   </div>
                 </div>
               </li>
@@ -29,15 +28,18 @@ export default function ProductsList() {
           : products.map((item) => (
               <li
                 key={item._id}
-                className="p-2 w-full min-[550px]:w-1/2 md:w-1/3 xl:w-1/4 2xl:w-1/5 grid"
+                className="p-2 w-full max-w-[300px] min-[550px]:max-w-full min-[550px]:w-1/2 md:w-1/3 xl:w-1/4 2xl:w-1/5 grid"
               >
-                <div className="shadow-lg border border-primary flex flex-col">
-                  <div className="border-b border-primary">
+                <div className="shadow-lg border border-border flex flex-col min-[550px]:w-full">
+                  <div className="border-b border-border">
                     <Image
-                      src={Picture}
-                      alt="Picture"
-                      className="object-cover max-h-60 lg:max-h-80 2xl:max-h-full"
+                      width={250}
+                      height={300}
                       priority
+                      quality={100}
+                      src={'/' + item.url}
+                      alt="Sneakers picture"
+                      className="object-cover max-h-[300px] block w-full bg-primary/10"
                     />
                   </div>
                   <div className="p-2 my-auto">
