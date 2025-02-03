d53a8fe36815708a05375b6b31d030444fe865ca
aquazor <kornevskiy99@gmail.com>
2025-02-04 00:42:57 +0200

    feat: implement useClickOutside hook

---
+++ src/app/hooks/useClickOuside.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export function useClickOutside(
+  enabled: boolean,
+  ref: React.RefObject<HTMLElement | null>,
+  callback: () => void
+) {
+  useEffect(() => {
+    if (!enabled || ref === null) {
+      return;
+    }
+
+    const handler = (event: MouseEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+
+      callback();
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler, true);
+    };
+  }, [ref, enabled, callback]);
+}
