32b0b54e899a04a2631025e4acf499388310767e
aquazor <kornevskiy99@gmail.com>
2025-02-05 02:23:01 +0200

    ref: change home page style

--- src/app/(with-appbar)/page.tsx
+++ src/app/(with-appbar)/page.tsx
@@ -1,9 +1,9 @@
-import Filters from './Filters';
-import ProductsList from './ProductsList';
+import Filters from './components/Filters';
+import ProductsList from './components/ProductsList';
 
 export default function Home() {
   return (
-    <div className="p-4 max-w-[1800px] mx-auto">
+    <div className="p-4 max-w-[112.5rem] mx-auto">
       <div className="flex lg:gap-2">
         <Filters />
         <ProductsList />
