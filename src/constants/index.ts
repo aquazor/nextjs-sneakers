22beef1736be19748363b15a913369b32e00e4be
aquazor <kornevskiy99@gmail.com>
2025-02-07 22:19:42 +0200

    feat: add general constants and types, add parsePrice util function

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const MIN_PRICE = 0;
+export const MAX_PRICE = 5000;
+export const MIN_PRICE_STR = MIN_PRICE.toString();
+export const MAX_PRICE_STR = MAX_PRICE.toString();
+export const STEP = 50;
+
+export const BRANDS = ['Adidas', 'Asics', 'Puma', 'Converse', 'Reebok', 'Vans', 'Veja'];
+export const SIZES = ['39', '40', '41', '42', '43', '44'];
+
+export const LIMIT = 10;
+export const LIMIT_STR = LIMIT.toString();
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { MAX_PRICE, MIN_PRICE } from '@/constants';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parsePrice(price: string | null, defaultPrice: number): string {
+  if (!price) return defaultPrice.toString();
+
+  const parsedPrice = Number(price);
+
+  return isNaN(parsedPrice) || parsedPrice < MIN_PRICE || parsedPrice > MAX_PRICE
+    ? defaultPrice.toString()
+    : parsedPrice.toString();
 }
