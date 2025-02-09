edb8e646bf1d62262a7a95e90a1e29f34f339d14
aquazor <kornevskiy99@gmail.com>
2025-02-09 23:43:12 +0200

    feat: implement loader for Item page

---
+++ src/app/(with-appbar)/sneakers/[itemId]/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from '@/components/Spinner';
+
+export default function ItemPageLoading() {
+  return (
+    <div className="flex items-center justify-center p-5">
+      <Spinner />
+    </div>
+  );
+}
