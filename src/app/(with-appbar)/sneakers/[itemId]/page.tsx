721e060cde3091cdc673363e052a168003417677
aquazor <kornevskiy99@gmail.com>
2025-02-09 23:44:34 +0200

    feat: implement Item page

---
+++ src/app/(with-appbar)/sneakers/[itemId]/page.tsx
@@ -0,0 +1,35 @@
+import { fetchItemById } from '@/lib/api/sneakers';
+import Swiper from './components/Swiper';
+import ItemInfo from './components/ItemInfo';
+import ItemDescription from './components/ItemDescription';
+
+export default async function ItemPage({
+  params,
+}: {
+  params: Promise<{ itemId: string }>;
+}) {
+  const { itemId } = await params;
+  const { item } = await fetchItemById({ itemId });
+
+  return (
+    <div className="p-4 max-w-[112.5rem] mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-[auto,1fr] md:grid-rows-[auto,1fr] gap-6">
+        <h2 className="block md:hidden text-4xl first-letter:text-5xl text-balance first-letter:font-bold">
+          {item.name}
+        </h2>
+
+        <div className="lg:row-span-2 flex md:block justify-center">
+          <Swiper images={Array(7).fill(item.url)} />
+        </div>
+
+        <div className="w-full max-w-[30rem] md:max-w-full mx-auto md:mx-0">
+          <ItemInfo item={item} />
+        </div>
+
+        <div className="col-span-1 md:col-span-2 lg:col-span-1 flex max-w-[30rem] md:max-w-full mx-auto md:mx-0">
+          <ItemDescription />
+        </div>
+      </div>
+    </div>
+  );
+}
