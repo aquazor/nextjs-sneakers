114ca09750f58064988cfe1a592c51eb2803304b
aquazor <kornevskiy99@gmail.com>
2025-02-09 23:44:08 +0200

    feat: implement route handler for Item page

---
+++ src/app/api/sneakers/[itemId]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import { MongooseError } from 'mongoose';
+import dbConnect from '@/lib/mongoose/dbConnect';
+import Product, { IProduct } from '@/lib/mongoose/models/ItemSchema';
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ itemId: string }> }
+) {
+  await dbConnect();
+
+  try {
+    const { itemId } = await params;
+
+    const item: IProduct | null = await Product.findById(itemId);
+
+    if (!item) {
+      return NextResponse.json({ message: 'Item not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ item });
+  } catch (err) {
+    console.log(err);
+
+    if (err instanceof MongooseError) {
+      return NextResponse.json({ message: err.message }, { status: 500 });
+    }
+
+    return NextResponse.json({ message: 'Something went wrong.' }, { status: 500 });
+  }
+}
---
+++ src/lib/api/sneakers.ts
@@ -0,0 +1,45 @@
+import { IProduct } from '../mongoose/models/ItemSchema';
+import { MAX_PRICE, MIN_PRICE } from '@/constants';
+import { UrlFilterParams } from '@/constants/types';
+
+export async function fetchItems(
+  { skip, limit, searchTerm, brands, sizes, minPrice, maxPrice, sort }: UrlFilterParams,
+  signal?: AbortSignal
+): Promise<{ items: IProduct[]; hasMore: boolean }> {
+  try {
+    const params = new URLSearchParams();
+    if (skip !== undefined) params.append('skip', skip);
+    if (limit !== undefined) params.append('limit', limit);
+    if (searchTerm) params.append('searchTerm', searchTerm);
+    if (brands) params.append('brands', brands);
+    if (sizes) params.append('sizes', sizes);
+    if (minPrice !== undefined && Number(minPrice) > MIN_PRICE)
+      params.append('minPrice', minPrice);
+    if (maxPrice !== undefined && Number(maxPrice) < MAX_PRICE)
+      params.append('maxPrice', maxPrice);
+    if (sort) params.append('sort', sort);
+
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/api/sneakers?${params.toString()}`;
+    const res = await fetch(url, { signal });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to fetch items');
+  }
+}
+
+export async function fetchItemById({
+  itemId,
+}: {
+  itemId: string;
+}): Promise<{ item: IProduct }> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/sneakers/${itemId}`);
+    const item = await res.json();
+    return item;
+  } catch (error) {
+    console.log(error);
+    throw new Error(`Failed to fetch item ${itemId}`);
+  }
+}
