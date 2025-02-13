311686b579c497d1b73427d211d6aabc70067bd0
aquazor <kornevskiy99@gmail.com>
2025-02-13 22:22:53 +0200

    feat: improve cart page, add loading state, add empty cart state

--- src/app/(with-appbar)/cart/page.tsx
+++ src/app/(with-appbar)/cart/page.tsx
@@ -1,29 +1,63 @@
 'use client';
 
-import { useCartContext } from '@/context/cartContext';
-import Container from '@/components/Container';
-import CartItem from './components/CartItem';
+import Image from 'next/image';
 import { IoBagCheckOutline } from 'react-icons/io5';
+import { useCartContext } from '@/context/cartContext';
+import EmptyCart from '@/assets/cart-empty.png';
+import Container from '@/components/Container';
+import CartItemList from './components/CartItemList';
+import Spinner from '@/components/Spinner';
 
 export default function CartPage() {
-  const { cartItems } = useCartContext();
-  const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.count, 0);
+  const { cartItems, isLoading } = useCartContext();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center py-5">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (cartItems.length === 0 && !isLoading) {
+    return (
+      <div className="w-full mt-10 flex flex-col items-center justify-center gap-6 text-center">
+        <div>
+          <h2 className="text-3xl">Your cart is empty</h2>
+          <div className="text-xl text-nowrap">¯\_(ツ)_/¯</div>
+        </div>
+        <h5 className="text-xl">Add at least one pair of sneakers to create an order.</h5>
+        <Image priority src={EmptyCart} quality={100} alt="Empty cart" />
+      </div>
+    );
+  }
+
+  const { price, count } = cartItems.reduce(
+    (acc, item) => {
+      const price = acc.price + item.price * item.count;
+      const count = acc.count + item.count;
+      return { price, count };
+    },
+    { price: 0, count: 0 }
+  );
 
   return (
     <Container>
-      <div className="flex gap-4 relative">
-        <ul className="w-full flex flex-wrap gap-4 *:w-full xl:*:w-[calc(50%-1rem/2)] bg-background">
-          {cartItems.map((item) => (
-            <CartItem key={item._id} item={item} />
-          ))}
-        </ul>
+      <div className="flex flex-col md:flex-row gap-4 relative">
+        <CartItemList />
 
-        <div className="sticky top-4 p-4 shrink-0 shadow-lg bg-background flex flex-col items-center justify-between mah-h-[50vh] gap-2 w-72">
-          <p className="text-xl">
-            TOTAL PRICE: <strong className="text-2xl">{totalPrice}</strong>{' '}
-          </p>
+        <div className="md:sticky top-4 py-2 px-4 shrink-0 items-center md:items-start shadow-lg border bg-background flex flex-col justify-between max-h-[calc(304px+1rem)] gap-4 md:w-60 lg:w-72">
+          <div className="space-y-2 text-center md:text-left">
+            <p className="text-xl">
+              TOTAL PRICE: <strong className="text-2xl">{price}</strong>
+            </p>
 
-          <button className="px-3 w-full py-2 flex items-center justify-center gap-1 transition-colors bg-primary text-primary-foreground font-bold text-lg">
+            <p className="text-lg">
+              Total items count: <strong className="text-xl">{count}</strong>
+            </p>
+          </div>
+
+          <button className="px-3 w-3/4 md:w-full py-2 flex items-center justify-center gap-1 transition-colors bg-primary text-primary-foreground font-bold text-lg">
             CHECKOUT <IoBagCheckOutline size={20} />
           </button>
         </div>
